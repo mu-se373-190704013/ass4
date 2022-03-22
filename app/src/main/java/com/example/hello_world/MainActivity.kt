7bdd2f89f3537e99748443bc151eb0eab928cc50
mtunahand <dgrmnc32@gmail.com>
2022-03-22 13:55:24 +0300

    commıt

--- app/src/main/java/com/example/hello_world/MainActivity.kt
+++ app/src/main/java/com/example/hello_world/MainActivity.kt
@@ -10,8 +10,18 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //lklkjlkj
 
+        val btnClick = findViewById<Button>(R.id.button1) //we define button
+        val textClick = findViewById<TextView>(R.id.textView) //we define click text
+        val textClick1 = findViewById<TextView>(R.id.textView2) // we define click text2
+        var timesClick = 0 // this is how many times clicked showing
+        btnClick.setOnClickListener{
+            btnClick.text = "I CLIKCED" // button text
+            textClick.text= "YOU CLICKED ME" //button text
+            timesClick = timesClick + 1 //when click it increases one by one
+            textClick1.text =timesClick.toString() // we converting an integer to a string
+
+        }
     }
 
 
